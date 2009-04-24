8c33114c21454623351081c1e6c027e77b23e14b
Johan Rydberg <johan.rydberg@gmail.com>
2009-04-24 15:21:46 +0200

    Split setup files for now.

--- setup.py
+++ setup.py
@@ -1,18 +1,7 @@
-"""
-Script for building the example.
-
-Usage:
-    python setup.py py2app
-"""
 from distutils.core import setup
 import py2app
 
 setup(
-    name="ScopeBarExample",
-    app=["main.py"],
-    data_files=["ScopeBarExample.nib"],
+    name="MGScopeBar",
     packages=["MGScopeBar"],
-    options=dict(py2app=dict(
-            plist="ScopeBarExample.plist",
-            )),
 )
